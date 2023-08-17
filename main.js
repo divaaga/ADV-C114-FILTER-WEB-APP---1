f3cb33e09b49b55d04a050772bf0a9250eff9312
divaaga <93255055+divaaga@users.noreply.github.com>
2023-08-17 17:22:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.centre();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save("my_filter_selfie.png");
+}
+
